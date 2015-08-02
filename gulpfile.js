3868b3658280aef24e94f96a616d97bd3af94a35
j-hannes <Hannes_E@gmx.de>
2015-08-02 15:32:02 +0200

    Add task automation via gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp    = require('gulp')
+var connect = require('gulp-connect')
+var elm     = require('gulp-elm')
+var watch   = require('gulp-watch')
+
+var del         = require('del')
+var runSequence = require('run-sequence')
+
+gulp.task('clean', function(cb) {
+    del(['public'], cb)
+})
+
+gulp.task('html', function() {
+    gulp.src('src/html/index.html')
+        .pipe(gulp.dest('public'))
+        .pipe(connect.reload())
+})
+
+gulp.task('elm', function() {
+    gulp.src('src/elm/app.elm')
+        .pipe(elm())
+        .pipe(gulp.dest('public'))
+        .pipe(connect.reload())
+})
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'public',
+        livereload: true
+    })
+})
+
+gulp.task('watch', function() {
+    gulp.watch('src/elm/app.elm', ['elm'])
+    gulp.watch('src/html/index.html', ['html'])
+})
+
+gulp.task('build', function() {
+    runSequence('clean', ['html', 'elm'])
+})
+
+gulp.task('dev', ['build', 'watch', 'connect'])
---
+++ readme.md
@@ -0,0 +1,4 @@
+# My first elm app
+
+Application to follow along
+https://github.com/j-hannes/my-first-elm-app/blob/master/docs/tutorial.md.
